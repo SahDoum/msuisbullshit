3a18ce12c758616face3e9408f0efdb25cb16c2c
SahDoum <sahdoum@gmail.com>
2017-10-12 23:34:41 +0300

    Update botForMSU.user.js

--- botForMSU.user.js
+++ botForMSU.user.js
@@ -17,7 +17,7 @@ function MyPixelBot() {
 
   MyPixelBot.url = {
     script: 'https://github.com/askesis/bot/raw/master/botForMSU.user.js',
-    image: 'https://i.imgur.com/C2BiBW3.png',
+    image: 'https://raw.githubusercontent.com/SahDoum/msuisbullshit/master/masked_msu_bordered.png',
   };
 
   MyPixelBot.refreshTime = 300;
